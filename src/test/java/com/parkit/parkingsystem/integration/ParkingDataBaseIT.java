86d96cbbbd19c6d8109be6b031c5e2a978bb833e
Houda Ben Abdallah <ahhouda@yahoo.fr>
2024-11-16 15:06:50 +0100

    ajout test dintegration testParkingLotExit()

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,6 +1,5 @@
 package com.parkit.parkingsystem.integration;
 
-import com.parkit.parkingsystem.constants.DBConstants;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
@@ -18,14 +17,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static junit.framework.Assert.*;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -39,7 +42,7 @@ public class ParkingDataBaseIT {
     private static InputReaderUtil inputReaderUtil;
 
     @BeforeAll
-    private static void setUp() throws Exception{
+    private static void setUp() throws Exception {
         parkingSpotDAO = new ParkingSpotDAO();
         parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
         ticketDAO = new TicketDAO();
@@ -57,16 +60,16 @@ public class ParkingDataBaseIT {
     }
 
     @AfterAll
-    private static void tearDown(){
-
+    private static void tearDown() {
     }
 
     @Test
     public void testParkingACar() throws Exception {
+        dataBasePrepareService.clearDataBaseEntries();
+
         // GIVEN
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, true);
-        dataBasePrepareService.clearDataBaseEntries();
 
         // WHEN
         parkingService.processIncomingVehicle();
@@ -84,11 +87,13 @@ public class ParkingDataBaseIT {
 
             // Verifie que le ticket a enregistre dans la bdd
             ResultSet rsTicket = psTicket.executeQuery();
-            assertTrue(rsTicket.next(), "Le ticket n'a pas été enregistré dans la base de données.");
+            assertTrue(rsTicket.next());
+            //assertTrue(ticketDAO.saveTicket(ticket));
 
             // Verifie que la place de parking est occupée (AVAILABLE = false)
             ResultSet rsParkingSpot = psParkingSpot.executeQuery();
-            assertTrue(rsParkingSpot.next(), "La place de parking n'a pas été trouvée dans la base de données.");
+            assertTrue(rsParkingSpot.next());
+
 
             // La place de parking devrait etre marque comme "occupee" (AVAILABLE = false)
             boolean isAvailable = rsParkingSpot.getBoolean("AVAILABLE");
@@ -103,13 +108,72 @@ public class ParkingDataBaseIT {
     }
 
 
-//    @Test
-//    public void testParkingLotExit(){
-//        testParkingACar();
-//        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-//        parkingService.processExitingVehicle();
-//        //TODO: check that the fare generated and out time are populated correctly in the database
-    //vérifier que le tarif généré et l'heure de départ sont correctement renseignés dans la base de données
-//    }
+
+
+    @Test
+    public void testParkingLotExit() throws Exception {
+
+        // GIVEN
+        testParkingACar();
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        System.out.println("/////////temps de stationnement = 45 minute//////////");
+        Date inTime = new Date();
+        inTime.setTime(System.currentTimeMillis() - (45 * 60 * 1000));//45 minutes parking
+        Date outTime = new Date();
+        //temps d'embarquement en minute
+        System.out.println("temps de stationnement est : " + (outTime.getTime() - inTime.getTime())/(1000*60));
+        System.out.println("///////////////////");
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        // THEN
+        parkingService.processExitingVehicle();
+
+        // WHEN
+        Connection con = dataBaseTestConfig.getConnection();
+        // Format des dates pour la requete SQL
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        // Format des dates en chaînes
+        String formattedInTime = dateFormat.format(inTime);
+        String formattedOutTime = dateFormat.format(outTime.getTime());
+
+        // Requete SQL pour mettre à jour les heures d'entrée et de sortie
+        String timeSql = "UPDATE ticket  IN_TIME = ?  OUT_TIME = ? "
+                + "WHERE VEHICLE_REG_NUMBER = 'ABCDEF'";
+        //Requete pour recuper le prix de stationnement
+        String priceSql = "SELECT PRICE FROM ticket WHERE VEHICLE_REG_NUMBER = 'ABCDEF' and PARKING_NUMBER = 1";
+
+        try {
+            // Envoi des requetes
+            PreparedStatement psTime = con.prepareStatement(timeSql);
+
+            //execute temps
+            psTime.setString(1, formattedInTime);
+            psTime.setString(2, formattedOutTime);
+
+            //assertTrue(psTime.execute());
+
+            PreparedStatement psPrice = con.prepareStatement(priceSql);
+            // Voir le prix de ticket
+            ResultSet rsPrice = psPrice.executeQuery();
+
+            System.out.println("***********************");
+
+            // Verification du prix
+            if (rsPrice.next()) {
+                double price = rsPrice.getDouble("PRICE");
+                System.out.println("Prix du ticket : " + price);
+                // Assertion : Le prix calculé doit être égal à 1.5 * 0.75
+                assertEquals(1.5 * 0.75, price);
+                System.out.println("************************");
+            }
+
+    } catch (SQLException e) {
+            e.printStackTrace();
+            dataBaseTestConfig.closeConnection(con);
+        } finally {
+            dataBaseTestConfig.closeConnection(con);
+        }
+    }
 
 }
